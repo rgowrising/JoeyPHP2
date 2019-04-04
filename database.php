1ace01f14bc7becc2cf43a16d30e146cf3d6a7bb
rocky <rocky-gow@hotmail.com>
2019-04-04 13:48:35 +0200

    database table php

---
+++ database.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>database</title>
+    <style>
+
+    </style>
+</head>
+<body>
+<?php
+$dbc = mysqli_connect('localhost','root','root','mijnschool') or die ('Error connecting.');
+$query = "SELECT * FROM klasgenoten";
+$result = mysqli_query($dbc,$query) or die ('Error querying.');
+while ($row = mysqli_fetch_array($result)){
+    echo $row['klasgenoot_id'].'/'.$row['voornaam'].'/'.$row['tussenvoegsel'].'/'.$row['achternaam'].'/'.$row['woonplaats'].'<br>';
+}
+?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
-echo 'indexPHP';
+echo 'index PHP';
 ?>
-
+<style>
+    a{
+        padding:10px;
+        color:black;
+    }
+</style>
 <a href="opdracht2.php">Opdracht 2</a>
 <a href="opdracht31.php">opdracht 31</a>
 <a href="opdracht32.php">opdracht 32</a>
@@ -12,3 +17,9 @@ echo 'indexPHP';
 <a href="opdracht51.html">opdracht 51</a>
 <a href="opdracht52.php">opdracht 52</a>
 <a href="opdracht61.php">opdracht 61</a>
+<a href="home.html">home</a>
+<a href="kenmerk.html">kenmerk</a>
+<a href="css.html">CSS</a>
+<a href="html.html">OPDRACHT</a>
+<a href="Opdrachtcss.html">grid</a>
+<a href="database.php">database</a>
